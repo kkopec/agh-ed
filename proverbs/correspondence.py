b238c37b29d68d1063552f99f464be37c58a0912
kkopec <kopeckrzysztoff@gmail.com>
2017-12-03 19:02:37 +0100

    Add day to month comparison implementation
    Added base class for day to month correspondence testing.
    It checks if the parameters trend is the same (more/less than mean value)
    and if day/month differences are similar.

--- proverbs/correspondence.py
+++ proverbs/correspondence.py
@@ -21,6 +21,70 @@ class Correspondence(Proverb):
         pass
 
 
+class Day2Month(Correspondence):
+    DAY = None
+    DAY_MONTH = 12
+    MONTH = None
+
+    NEXT_YEAR = True
+    MIN_SCORE = 2
+    MIN_DIFF = 1
+
+    def build_filter(self, year):
+        return lambda sheet: (
+            sheet[(sheet['DATE'] == '{0}-{1:02d}-{2:02d}'.format(year, self.DAY_MONTH, self.DAY))],
+            sheet[((sheet['DATE'] >= '{0}-{1:02d}'.format(year+self.NEXT_YEAR, self.MONTH)) &
+                   (sheet['DATE'] < '{0}-{1:02d}'.format(year+self.NEXT_YEAR, self.MONTH+1)))])
+
+    def process_year(self, place, year):
+        score = 0
+        params_tested = 0
+        for param in Param.all():
+            day = self.get_day_data(place, year, param)
+            month = self.get_month_data(place, year, param=param)
+
+            if len(day) == 0 or len(month) == 0:
+                continue
+
+            day = day[0]
+            month = np.mean(month)
+
+            score += self.compare_day_to_month(place, param, day, month)
+            params_tested += 1
+
+        return score >= self.MIN_SCORE if params_tested >= 2 else None
+
+    def prepare_data(self):
+        self.data['mean'] = {}
+        for place in self.data_provider.get_places():
+            self.data['mean'][place] = {
+                'day': {},
+                'month': {}
+            }
+            for param in Param.all():
+                param_data_day = reduce((lambda x, y: x + y), [self.get_day_data(place, y, param) for y in self.data[place].keys()])
+                param_data_month = reduce((lambda x, y: x + y), [self.get_month_data(place, y, param=param) for y in self.data[place].keys()])
+
+                self.data['mean'][place]['day'][param] = np.mean(param_data_day)
+                self.data['mean'][place]['month'][param] = np.mean(param_data_month)
+
+    def get_day_data(self, place, year, param, index=0):
+        sheet = self.data[place][year][index]
+        return [float(x) for x in list(sheet[(~sheet[param].isin(['-']))][param])]
+
+    def get_month_data(self, place, year, param=Param.TAVG.value, index=1):
+        return super().get_month_data(place, year, param, index)
+
+    def compare_day_to_month(self, place, param, day, month):
+        """
+        same trend and similar differences
+        """
+        day_diff = day - self.data['mean'][place]['day'][param]
+        month_diff = np.mean(month) - self.data['mean'][place]['month'][param]
+
+        return day_diff * month_diff >= 0 and abs(day_diff - month_diff) <= self.MIN_DIFF
+
+
 class Month2Month(Correspondence):
     FIRST_MONTH = None
     SECOND_MONTH = None
