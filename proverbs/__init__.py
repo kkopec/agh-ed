b1f60c6450b78bd3554ba9616e9a36b050fee08b
kkopec <kopeckrzysztoff@gmail.com>
2017-12-03 19:06:40 +0100

    Add proverbs related to days before christmas eve
    Added set of proverbs based on day-to-month's logic.

--- proverbs/__init__.py
+++ proverbs/__init__.py
@@ -4,3 +4,16 @@ from proverbs.kwiecien import Kwiecien
 from proverbs.luty_cieply import LutyCieply
 from proverbs.styczen_wiosna import StyczenWiosna
 from proverbs.zoska import ZimnaZoska
+from proverbs.wigilia_miesiace import \
+    D2M13Styczen,\
+    D2M14Luty,\
+    D2M15Marzec,\
+    D2M16Kwiecien,\
+    D2M17Maj,\
+    D2M18Czerwiec,\
+    D2M19Lipiec,\
+    D2M20Sierpien, \
+    D2M21Wrzesien,\
+    D2M22Pazdziernik,\
+    D2M23Listopad,\
+    D2M24Grudzien
---
+++ proverbs/wigilia_miesiace.py
@@ -0,0 +1,133 @@
+from proverbs.correspondence import Day2Month
+
+
+class D2M13Styczen(Day2Month):
+    """
+    Przed wigilią: 13 Grudzień jak Styczeń
+    """
+    DAY = 13
+    MONTH = 1
+
+    def __repr__(self):
+        return u"Przed wigilią: 13 Grudzień jak Styczeń"
+
+
+class D2M14Luty(Day2Month):
+    """
+    Przed wigilią: 14 Grudzień jak Luty
+    """
+    DAY = 14
+    MONTH = 2
+
+    def __repr__(self):
+        return u"Przed wigilią: 14 Grudzień jak Luty"
+
+
+class D2M15Marzec(Day2Month):
+    """
+    Przed wigilią: 15 Grudzień jak Marzec
+    """
+    DAY = 15
+    MONTH = 3
+
+    def __repr__(self):
+        return u"Przed wigilią: 15 Grudzień jak Marzec"
+
+
+class D2M16Kwiecien(Day2Month):
+    """
+    Przed wigilią: 16 Grudzień jak Kwiecień
+    """
+    DAY = 16
+    MONTH = 4
+
+    def __repr__(self):
+        return u"Przed wigilią: 16 Grudzień jak Kwiecień"
+
+
+class D2M17Maj(Day2Month):
+    """
+    Przed wigilią: 17 Grudzień jak Maj
+    """
+    DAY = 17
+    MONTH = 5
+
+    def __repr__(self):
+        return u"Przed wigilią: 17 Grudzień jak Maj"
+
+
+class D2M18Czerwiec(Day2Month):
+    """
+    Przed wigilią: 18 Grudzień jak Czerwiec
+    """
+    DAY = 18
+    MONTH = 6
+
+    def __repr__(self):
+        return u"Przed wigilią: 18 Grudzień jak Czerwiec"
+
+
+class D2M19Lipiec(Day2Month):
+    """
+    Przed wigilią: 19 Grudzień jak Lipiec
+    """
+    DAY = 19
+    MONTH = 7
+
+    def __repr__(self):
+        return u"Przed wigilią: 19 Grudzień jak Lipiec"
+
+
+class D2M20Sierpien(Day2Month):
+    """
+    Przed wigilią: 20 Grudzień jak Sierpień
+    """
+    DAY = 20
+    MONTH = 8
+
+    def __repr__(self):
+        return u"Przed wigilią: 20 Grudzień jak Sierpień"
+
+
+class D2M21Wrzesien(Day2Month):
+    """
+    Przed wigilią: 21 Grudzień jak Wrzesień
+    """
+    DAY = 21
+    MONTH = 9
+
+    def __repr__(self):
+        return u"Przed wigilią: 21 Grudzień jak Wrzesień"
+
+
+class D2M22Pazdziernik(Day2Month):
+    """
+    Przed wigilią: 22 Grudzień jak Październik
+    """
+    DAY = 22
+    MONTH = 10
+
+    def __repr__(self):
+        return u"Przed wigilią: 22 Grudzień jak Październik"
+
+
+class D2M23Listopad(Day2Month):
+    """
+    Przed wigilią: 23 Grudzień jak Listopad
+    """
+    DAY = 23
+    MONTH = 11
+
+    def __repr__(self):
+        return u"Przed wigilią: 23 Grudzień jak Listopad"
+
+
+class D2M24Grudzien(Day2Month):
+    """
+    Przed wigilią: 24 Grudzień jak Grudzień
+    """
+    DAY = 14
+    MONTH = 12
+
+    def __repr__(self):
+        return u"Przed wigilią: 24 Grudzień jak Grudzień"
